8f94eb98f445ef631b54f3640ed649d788862def
PingHung Lee <henrylee2222z@gmail.com>
2026-06-30 15:06:34 +0800

    feat: 新增 docker-compose.yml 並實驗 volumes 機制

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ print("Hello from inside a Docker container!")
 import platform
 print(f"Python version: {platform.python_version()}")
 print(f"System: {platform.system()}")
+print("這行是新加的，沒有重新 build")
